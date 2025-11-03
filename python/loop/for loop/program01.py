eede54d62311dc840ecd5e46644446ecfc63417d
Daleepswami <daleepswami22170@gmail.com>
2025-11-04 00:05:32 +0530

    Update program01.py

--- python/loop/for loop/program01.py
+++ python/loop/for loop/program01.py
@@ -1,1 +1,8 @@
-
+#take a input a number and find which numbers divide it and how many number can divide it.
+num=int(input("Enter a nuber"))
+count=0
+for i in range(1,num+1):
+    if num%i==0:
+       count+=1
+       print(f"{num} is divide by {i}")
+    
